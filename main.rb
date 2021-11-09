202e4e181564e9b016b9b86f2a6cd13456643b4c
Karine Vieira <kv.vieira1@gmail.com>
2021-11-08 21:01:23 -0300

    refatoracao

--- main.rb
+++ main.rb
@@ -1,12 +1,23 @@
-def verificar_se_acertou(palavra, chute, acertos, erros)
-	for letra in 0..palavra.size
-		if (palavra[letra] == chute)
-			acertos << palavra[letra]
-			puts "Acertos até agora #{acertos}"
-			break
-		end
-	end
-	erros << chute
+def da_boas_vindas
+	puts "Bem-vindo ao Jogo da Forca"
+	puts "Qual é o seu nome?"
+	nome = gets.strip
+	puts "Vamos iniciar o jogo para você, #{nome}"
+	puts "\n"
+end
+
+def escolher_palavra
+	puts "Escolhendo uma palavra..."
+	palavra = "programador"
+	puts "A palavra escolhida tem #{palavra.size} letras, boa sorte!"
+	puts "\n"
+	palavra
+end
+
+def pedir_chute
+	puts "Agora insira uma letra ou uma palavra: "
+	chute = gets.strip
+	chute
 end
 
 def iniciar_jogo
@@ -14,13 +25,38 @@ def iniciar_jogo
 	palavra = escolher_palavra
 	acertos = []
 	erros = []
+	pontos = 1000
 	tentativas = 5
 	
 	for tentativa in 1..tentativas
 		puts "Tentativa #{tentativa}"
-		chute = pedir_chute acertos, erros
-		verificar_se_acertou palavra, chute, acertos, erros
+		puts "Acertos até agora: #{acertos}"
+		puts "Erros até agora: #{erros}"
+		chute = pedir_chute 
+		chutou_uma_letra = chute.size == 1
+
+		if chutou_uma_letra
+			letra_procurada = chute[0]
+			total_encontrado = palavra.count letra_procurada
+			if total_encontrado == 0
+				erros << letra_procurada
+				puts "Você não encontrou nenhuma letra."
+				pontos -= 200
+			else
+				acertos << letra_procurada
+				puts "Você encontrou #{total_encontrado} letras."
+			end
+		else
+			if palavra == chute
+				puts "Parabéns, você acertou!"
+				break
+			else
+				pontos -= 200
+				puts "Não foi dessa vez... tente novamente"
+			end
+		end
 	end
+puts "Você fez #{pontos}."
 end
 
 def nao_quer_jogar?
