14981bb0ec647639e81cdbc3f5554d8bb1c14337
Karine Vieira <kv.vieira1@gmail.com>
2021-11-17 08:13:58 -0300

    add dicionario.txt and fix indentation

--- main.rb
+++ main.rb
@@ -1,70 +1,80 @@
 require_relative 'ui'
 
+def escolher_palavra
+  aviso_escolhendo_palavra
+  palavras = File.read("dicionario.txt")
+  todas_as_palavras = palavras.split "\n"
+  numero_escolhido = rand(todas_as_palavras.size)
+  palavra = todas_as_palavras[numero_escolhido]
+  aviso_palavra_escolhida(palavra)
+  palavra
+end
+
 def palavra_mascarada(palavra, chutes)
-	mascara = ""
-	for letra in palavra.chars
-		if chutes.include? letra
-    		mascara += letra
-    	else
-    		mascara += "_"
-    	end
-	end
-  	mascara
+  mascara = ""
+  for letra in palavra.chars
+    if chutes.include? letra
+      mascara += letra
+    else
+      mascara += "_"
+    end
+  end
+  mascara
 end
 
 def iniciar_jogo
-	da_boas_vindas
-	palavra = escolher_palavra
-	chutes = []
-	erros = 0
-	pontos = 0
-	tentativas = 5
+  da_boas_vindas
+  palavra = escolher_palavra
+  chutes = []
+  erros = 0
+  pontos = 0
+  tentativas = 5
 	
   for tentativa in 1..tentativas
-	mascara = palavra_mascarada(palavra, chutes)
+    mascara = palavra_mascarada(palavra, chutes)
     chute = pedir_chute(chutes, erros, tentativa, mascara)
     
-		if chutes.include? chute
-			aviso_chute_repetido(chute)
-			next
-    	end
+	if chutes.include? chute
+	  aviso_chute_repetido(chute)
+	  next
+    end
     
-		chutes << chute 
-		chutou_uma_letra = chute.size == 1
+	chutes << chute 
+	chutou_uma_letra = chute.size == 1
 
-		if chutou_uma_letra
-			letra_procurada = chute[0]
-			total_encontrado = palavra.count letra_procurada
-			if total_encontrado == 0
-				erros += 1
-				pontos -= 30
-			else
-				pontos += 100
-			end
-		else
-			if palavra == chute
-				aviso_palavra_encontrada
-				pontos = 500
-				break
-			else
-				pontos -= 30
-				aviso_palavra_nao_encontrada
-			end
-		end
+	if chutou_uma_letra
+	  letra_procurada = chute[0]
+	  total_encontrado = palavra.count letra_procurada
+	  if total_encontrado == 0
+		erros += 1
+		pontos -= 30
+	  else
+		pontos += 100
+	  end
+	else
+	  if palavra == chute
+		aviso_palavra_encontrada
+		pontos = 500
+		break
+	  else
+		pontos -= 30
+		aviso_palavra_nao_encontrada
+	  end
 	end
+  end
 aviso_pontuacao(pontos)
 end
 
 def nao_quer_jogar?
-	aviso_jogar_novamente
-	quer_jogar = gets.strip
-	quer_jogar.upcase == "N"
+  aviso_jogar_novamente
+  quer_jogar = gets.strip
+  quer_jogar.upcase == "N"
 end
 
 loop do 
-	iniciar_jogo
-	if nao_quer_jogar?
-		aviso_jogo_finalizado
-		break
-	end
+  iniciar_jogo
+  if nao_quer_jogar?
+    aviso_jogo_finalizado
+	break
+  end
 end
--- ui.rb
+++ ui.rb
@@ -1,48 +1,49 @@
 def da_boas_vindas
-	puts "Bem-vindo ao Jogo da Forca"
-	puts "Qual é o seu nome?"
-	nome = gets.strip
-	puts "Vamos iniciar o jogo para você, #{nome}"
-	puts "\n"
-end
-
-def escolher_palavra
-	puts "Escolhendo uma palavra..."
-	palavra = "programador"
-  	puts "A palavra escolhida tem #{palavra.size} letras, boa sorte!"
-	puts "\n"
-	palavra
+  puts "Bem-vindo ao Jogo da Forca"
+  puts "Qual é o seu nome?"
+  nome = gets.strip
+  puts "Vamos iniciar o jogo para você, #{nome}"
+  puts "\n"
 end
 
 def pedir_chute(chutes, erros, tentativa, mascara)
-  	puts "Tentativa #{tentativa}"
-  	puts "Palavra: #{mascara}"
-  	puts "Chutes até agora: #{chutes}"
-  	puts "Agora insira uma letra ou uma palavra: "
-  	chute = gets.strip
-  	chute
+  puts "Tentativa #{tentativa}"
+  puts "Palavra: #{mascara}"
+  puts "Chutes até agora: #{chutes}"
+  puts "Agora insira uma letra ou uma palavra: "
+  chute = gets.strip
+  chute
+end
+
+def aviso_escolhendo_palavra
+  puts "Escolhendo uma palavra..."
+end
+
+def aviso_palavra_escolhida(palavra)
+  puts "A palavra escolhida tem #{palavra.size} letras, boa sorte!"
+  puts "\n"
 end
 
 def aviso_chute_repetido(chute)
-  	puts "Você já chutou #{chute}"
+  puts "Você já chutou #{chute}"
 end
 
 def aviso_palavra_encontrada
-	puts "Parabéns, você acertou!"
+  puts "Parabéns, você acertou!"
 end
 
 def aviso_palavra_nao_encontrada
-  	puts "Não foi dessa vez... tente novamente"
+  puts "Não foi dessa vez... tente novamente"
 end
 
 def aviso_pontuacao(pontos)
-  	puts "Você fez #{pontos} pontos."
+  puts "Você fez #{pontos} pontos."
 end
 
 def aviso_jogar_novamente
-  	puts "Deseja jogar novamente? (S/N)"
+  puts "Deseja jogar novamente? (S/N)"
 end
 
 def aviso_jogo_finalizado
-  	puts "Jogo finalizado"
+  puts "Jogo finalizado"
 end
